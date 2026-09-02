e04868789cce81959365625dbca4e052bbecb4fb
Tamanleen-kaur <tamanleenkaur13@gmail.com>
2026-09-02 17:28:42 +0530

    Day02 Check Even Odd
    Add a script to check if a number is even or odd.

---
+++ Day02-Check-Even-Odd.py
@@ -0,0 +1,8 @@
+#Check Even and Odd Number
+num=int(input("enter number:"))
+if num%2==0:
+    print(num,"is even")
+else:
+    print(num,"is odd")
+  #Time complexity:O(1)
+  #Space complexity:O(1)
