80145da0ce596016021dbaa341636d07f0189966
Tamanleen-kaur <tamanleenkaur13@gmail.com>
2026-09-04 11:56:00 +0530

    Day-4-Prime Check
    Implement prime number check with user input.

---
+++ Day04-Check-the-prime-number.py
@@ -0,0 +1,12 @@
+#DAY-4-Prime Check
+num=int(input("Enter the number:"))
+count=0
+for i in range(1,num+1):
+    if num%i==0:
+        count+=1
+if count==2:
+    print(num,"is prime number")
+else:
+    print(num,"is not prime")
+#Time complexity:O(n)
+#Space complexity:O(1)
