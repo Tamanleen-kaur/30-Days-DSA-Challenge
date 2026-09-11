dcdfa5a50b168e2bbb76edb0850653bf246be19e
Tamanleen-kaur <tamanleenkaur13@gmail.com>
2026-09-11 17:49:53 +0530

    Day11-Count-the-words

---
+++ Day11-Count-the-words.py
@@ -0,0 +1,9 @@
+#Day11-Count-the-words
+string="i love python" 
+max_length=0
+for ch in string:
+    if ch==" ":
+        max_length=max_length+1
+print(max_length+1)
+#Time complexity:O(n)
+#Space complexity:O(1)
